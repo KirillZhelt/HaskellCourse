95a6739e892d84de120df12dc0add1e623ac8c89
Kirill Zheltovski <kirillzhelt@gmail.com>
2020-09-23 22:59:15 +0300

    [task 2] Add unfolding into binary digits

--- second_task/second_task.hs
+++ second_task/second_task.hs
@@ -23,3 +23,14 @@ foldrX :: (a -> b -> b) -> b -> [a] -> b
 foldrX _ b [] = b
 foldrX f b (x:[]) = f x b
 foldrX f b (x:xs) = f x (foldrX f b xs)
+
+unfold :: (a -> Maybe (a, b)) -> a -> [b]
+unfold f x = maybe [] (\(u, v) -> v : (unfold f u)) (f x)
+
+binaryUnfolder :: Int -> Maybe (Int, Int)
+binaryUnfolder 0 = Nothing
+binaryUnfolder i = Just (div i 2, mod i 2)
+
+binaryDigits :: Int -> [Int]
+binaryDigits 0 = [0]
+binaryDigits i = reverse (unfold binaryUnfolder i)
